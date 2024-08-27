32c02c8b176012abce08c98b4b6097d870879ebb
PovilasU <povilas.urbonas85@gmail.com>
2024-08-27 20:34:51 +0300

    add controler , model

--- CargoLoadAPI/CargoLoadAPI.Server/Controllers/CargoLoadsController.cs
+++ CargoLoadAPI/CargoLoadAPI.Server/Controllers/CargoLoadsController.cs
@@ -1,6 +1,30 @@
-﻿namespace CargoLoadAPI.Server.Controllers
+﻿using CargoLoadAPI.Server.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CargoLoadAPI.Server.Controllers
 {
-    public class CargoLoadsController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CargoLoadsController : ControllerBase
     {
+        // In-memory storage for demonstration purposes
+        private static List<CargoLoad> CargoLoads = new List<CargoLoad>();
+
+        // GET: api/CargoLoads
+        [HttpGet]
+        public ActionResult<IEnumerable<CargoLoad>> Get()
+        {
+            return Ok(CargoLoads);
+        }
+
+        // POST: api/CargoLoads
+        [HttpPost]
+        public ActionResult Post([FromBody] CargoLoad load)
+        {
+            load.Id = CargoLoads.Count + 1;
+            CargoLoads.Add(load);
+            return Ok(load);
+        }
     }
 }
--- CargoLoadAPI/CargoLoadAPI.Server/Models/CargoLoad.cs
+++ CargoLoadAPI/CargoLoadAPI.Server/Models/CargoLoad.cs
@@ -2,5 +2,12 @@
 {
     public class CargoLoad
     {
+        public int Id { get; set; }
+        public string DriverName { get; set; }
+        public string VehicleNumber { get; set; }
+        public string VehicleType { get; set; }
+        public double LoadWeight { get; set; }
+        public bool IsDangerousGoods { get; set; }
     }
 }
+
