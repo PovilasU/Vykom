832a7e2a311ad6f8c61becc58df84b46e6764e2f
PovilasU <povilas.urbonas85@gmail.com>
2024-08-28 05:50:56 +0300

    updated logo

--- CargoLoadAPI/cargoloadapi.client/src/components/Navbar.jsx
+++ CargoLoadAPI/cargoloadapi.client/src/components/Navbar.jsx
@@ -4,34 +4,43 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="/">
-        <img src="/logo.jpg" alt="Logo" width="50" height="50" />
-      </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link className="nav-link" to="/">
-              Cargo Load Management
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/export">
-              Export Cargo Loads
-            </Link>
-          </li>
-        </ul>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand d-flex align-items-center" href="/">
+          <img
+            src="/logo.jpg"
+            alt="Logo"
+            width="50"
+            height="50"
+            className="d-inline-block align-top"
+          />
+          <span className="ms-3">Dziugas Logistics</span>
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                Cargo Load Management
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/export">
+                Export Cargo Loads
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
